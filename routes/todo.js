4154b94aa4c909d1eb0850d7dffc7d4b2d4ac855
adtbo <adtmustika@gmail.com>
2020-06-23 22:04:22 +0800

    adding Swagger API documentation

--- routes/todo.js
+++ routes/todo.js
@@ -1,24 +1,103 @@
 const router = require('express').Router();
 let Todo = require('../model/todo.model');
 
+/**
+ * @swagger
+ * definitions:
+ *  Task:
+ *   type: "object"
+ *   properties:
+ *    description:
+ *      type: "string"
+ *    doneDateTime:
+ *      type: "string"
+ *      format: "date-time"
+ */
+
+/**
+ * @swagger
+ * /task/:
+ *  get:
+ *    tags:
+ *    - "Task"
+ *    description: Get all task
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.route('/').get((req, res) => {
     Todo.find()
         .then(todos => res.json(todos))
         .catch(err => res.status(400).json('Error: ' + err));       
 });
 
+
+/**
+ * @swagger
+ * /task/{id}:
+ *  get:
+ *    tags:
+ *    - "Task"
+ *    description: Get specific task
+ *    parameters:
+ *    - name: "id"
+ *      in: "path"
+ *      description: "id of Task to return"
+ *      required: true
+ *      type: "string"
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.route('/:id').get((req, res) => {
     Todo.findById(req.params.id)
         .then(todos => res.json(todos))
         .catch(err => res.status(400).json('Error: ' + err));       
 });
 
+
+/**
+ * @swagger
+ * /task/{id}:
+ *  delete:
+ *    tags:
+ *    - "Task"
+ *    description: delete existing Task
+ *    parameters:
+ *    - name: "id"
+ *      in: "path"
+ *      description: "id of Task to delete"
+ *      required: true
+ *      type: "string"
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.route('/:id').delete((req, res) => {
     Todo.findByIdAndDelete(req.params.id)
         .then(todos => res.json('Task Deleted'))
         .catch(err => res.status(400).json('Error: ' + err));       
 });
 
+
+/**
+ * @swagger
+ * /task/:
+ *  post:
+ *    tags:
+ *    - "Task"
+ *    description: Create new Task
+ *    parameters:
+ *    - in: "body"
+ *      name: "body"
+ *      description: "task that will be created"
+ *      required: true
+ *      schema:
+ *        $ref: "#/definitions/Task"
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.route('/').post((req, res) => {
 
     const todo = {
@@ -33,6 +112,30 @@ router.route('/').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+
+/**
+ * @swagger
+ * /task/{id}:
+ *  post:
+ *    tags:
+ *    - "Task"
+ *    description: Update existing Task
+ *    parameters:
+ *    - name: "id"
+ *      in: "path"
+ *      description: "id of Task to update"
+ *      required: true
+ *      type: "string"
+ *    - in: "body"
+ *      name: "body"
+ *      description: "task that will be created"
+ *      required: true
+ *      schema:
+ *        $ref: "#/definitions/Task"
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.route('/:id').post((req, res) => {
     Todo.findById(req.params.id)
         .then(todo => {
---
+++ swaggerDoc.js
@@ -0,0 +1,23 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        version: "1.0.0",
+        title: "to do API",
+        description: "to do API Information",
+        contact: {
+          name: "Developer"
+        },
+        servers: ["http://localhost:3000"]
+      },
+    },
+    apis: ['routes/todo.js']
+};
+
+const specs = swaggerJsDoc(swaggerOptions);
+
+module.exports = (app) => {
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+}
